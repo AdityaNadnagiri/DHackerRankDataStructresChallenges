96c8d469dd085e0694b09a7ca56ffaaf5df34722
nanda <nanda@192.168.1.108>
2017-11-06 16:41:59 -0500

    v6

--- src/gJavaMap/Solution.java
+++ src/gJavaMap/Solution.java
@@ -1,24 +1,25 @@
 package gJavaMap;
+
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
-		HashMap<String,String> dir=new HashMap<>(); 
-		Scanner input=new Scanner(System.in);
-		int n=input.nextInt();
+		HashMap<String, String> dir = new HashMap<>();
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
 		input.nextLine();
-		for(int i=0;i<n;i++) {
-			String name=input.nextLine();
-			String num=input.nextLine();
+		for (int i = 0; i < n; i++) {
+			String name = input.nextLine();
+			String num = input.nextLine();
 			dir.put(name, num);
 		}
-		while(input.hasNext()) {
-			String s=input.nextLine();
-			if(dir.get(s)==null) {
+		while (input.hasNext()) {
+			String s = input.nextLine();
+			if (dir.get(s) == null) {
 				System.out.println("Not found");
-			}else{
-				System.out.println(s+"="+dir.get(s));
+			} else {
+				System.out.println(s + "=" + dir.get(s));
 			}
 		}
 		input.close();
